ef5e6d9bb91df72ebdcbbbebcc5b22933febef0d
ma <ma@192.168.2.36>
2018-07-02 21:35:43 -0700

    nn

---
+++ src/Multiplication.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String[] args) {
+		//Scanner s = new Scanner(System.in);
+		//int n = s.nextInt();
+		int n = 2;
+		int sum;
+		for(int i = 1; i <= 10; i++) {
+			sum = n * i;
+			System.out.println(n+              "*"  +i  +"="                          	 +sum);
+			
+			
+			
+		}
+	}
+}
